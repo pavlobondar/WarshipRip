5f78e1bbe5e25e465c7d442659e06016261c4376
Pavlo Bondar <ivizey@gmail.com>
2024-08-06 11:29:06 +0300

    Initial Commit

---
+++ WarshipRip/WarshipRipApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarshipRipApp.swift
+//  WarshipRip
+//
+//  Created by Pavlo on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WarshipRipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
